3e6613e7eebf95ed34ed186791b9869330136f36
Misengkang <hkk4641908@163.com>
2017-09-12 16:33:02 +0800

    add django-blog

--- newblog/newblog/settings.py
+++ newblog/newblog/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2v8swkzlupa5=y67w+qd(ydm0=p!fh*)847glbua3za(pfek8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.kangkangblog.site']
 
 # Application definition
 
@@ -118,3 +118,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
