98df2cbb6f6873f61326b9dd1e51239c2eb5ba1a
Misengkang <hkk4641908@163.com>
2017-09-15 11:04:31 +0800

    add urls.py views.py ---- add DetailView

--- newblog/blog/views.py
+++ newblog/blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from .models import Post, Category, Tag
 import markdown
 from comments.forms import CommentForm
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 
 # 原先版本1.0
@@ -166,26 +166,71 @@ class IndexView(ListView):  # ListView已经自动获取文章列表数据，并
 # 开发流程：首先配置 URL，即把相关的 URL 和视图函数绑定在一起，
 # 然后实现视图函数，编写模板并让视图函数渲染模板。
 
-def detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
+# detail视图原先版本
+# def detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#
+#     # 阅读量 +1
+#     post.increase_views()
+#
+#     # 用markdown格式渲染html
+#     post.body = markdown.markdown(post.body,
+#                                   extensions=[
+#                                       'markdown.extensions.extra',
+#                                       'markdown.extensions.codehilite',
+#                                       'markdown.extensions.toc',
+#                                   ])
+#     form = CommentForm()
+#     comment_list = post.comment_set.all()  # 获取稳这篇文章下全部评论
+#     context = {'post': post,  # 将文章，表单，以及评论作为模板变量传给detail.html
+#                'form': form,
+#                'comment_list': comment_list
+#                }
+#     return render(request, 'blog/detail.html', context=context)
 
-    # 阅读量 +1
-    post.increase_views()
+# 基于DetailView类的detail视图
+class PostDetailView(DetailView):
+    # 这些属性的含义和 ListView 是一样的
+    model = Post
+    template_name = 'blog/detail.html'
+    context_object_name = 'post'
 
-    # 用markdown格式渲染html
-    post.body = markdown.markdown(post.body,
-                                  extensions=[
-                                      'markdown.extensions.extra',
-                                      'markdown.extensions.codehilite',
-                                      'markdown.extensions.toc',
-                                  ])
-    form = CommentForm()
-    comment_list = post.comment_set.all()  # 获取稳这篇文章下全部评论
-    context = {'post': post,  # 将文章，表单，以及评论作为模板变量传给detail.html
-               'form': form,
-               'comment_list': comment_list
-               }
-    return render(request, 'blog/detail.html', context=context)
+    def get(self, request, *args, **kwargs):
+        # 覆写 get 方法的目的是因为每当文章被访问一次，就得将文章阅读量 +1
+        # get 方法返回的是一个 HttpResponse 实例
+        # 之所以需要先调用父类的 get 方法，是因为只有当 get 方法被调用后，
+        # 才有 self.object 属性，其值为 Post 模型实例，即被访问的文章 post
+        response = super(PostDetailView, self).get(request, *args, **kwargs)
+
+        # 将文章阅读量 +1
+        # 注意 self.object 的值就是被访问的文章 post
+        self.object.increase_views()
+
+        # 视图必须返回一个 HttpResponse 对象
+        return response
+
+    def get_object(self, queryset=None):
+        # 覆写 get_object 方法的目的是因为需要对 post 的 body 值进行渲染
+        post = super(PostDetailView, self).get_object(queryset=None)
+        post.body = markdown.markdown(post.body,
+                                      extensions=[
+                                          'markdown.extensions.extra',
+                                          'markdown.extensions.codehilite',
+                                          'markdown.extensions.toc',
+                                      ])
+        return post
+
+    def get_context_data(self, **kwargs):
+        # 覆写 get_context_data 的目的是因为除了将 post 传递给模板外（DetailView 已经帮我们完成），
+        # 还要把评论表单、post 下的评论列表传递给模板。
+        context = super(PostDetailView, self).get_context_data(**kwargs)
+        form = CommentForm()
+        comment_list = self.object.comment_set.all()
+        context.update({
+            'form': form,
+            'comment_list': comment_list
+        })
+        return context
 
 
 # 归档视图函数，根据文章发表年月来过滤，create_time是python的data对象，有一个year和month属性，
