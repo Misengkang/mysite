ce6e9ef42602e6f1913cd6be825907a3b90f0627
Misengkang <hkk4641908@163.com>
2017-09-19 13:36:25 +0800

    MODIFIED /SETTINGS.py   add upload pictures static

--- newblog/newblog/settings.py
+++ newblog/newblog/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '2v8swkzlupa5=y67w+qd(ydm0=p!fh*)847glbua3za(pfek8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.kangkangblog.site']
 
