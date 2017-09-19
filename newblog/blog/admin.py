3b3c176b73f6ef497b8275c960f64bc9baaa33d6
Misengkang <hkk4641908@163.com>
2017-09-19 18:31:17 +0800

    MODIFIED /admin.py

--- newblog/blog/admin.py
+++ newblog/blog/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
 
+admin.site.site_header = "Kangkang Blog"
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'create_time', 'modified_time', 'category', 'author']
+    list_filter = ['create_time']
+    search_fields = ['body']
 
 
 admin.site.register(Post, PostAdmin)
