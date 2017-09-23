9cfb3550ca8d29e9abd3debd3ca80ef4214132a7
Misengkang <hkk4641908@163.com>
2017-09-23 08:43:48 +0800

    add a form

--- newblog/guestbook/views.py
+++ newblog/guestbook/views.py
@@ -14,10 +14,9 @@ def message(request):
 def save(request):
     visitor_name = request.POST.get("visitor_name")
     visitor_email = request.POST.get("visitor_email")
-    visitor_url = request.POST.get("visitor_url")
     message_text = request.POST.get("message_text")
     message = Message(visitor_name=visitor_name, visitor_email=visitor_email,
-                      visitor_url=visitor_url, message_text=message_text)
+                      message_text=message_text)
     message.save()
 
     return HttpResponseRedirect('/message/')
